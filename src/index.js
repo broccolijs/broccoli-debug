24b629ac449b7fdf1609d182ee57c85fa465c79e
Robert Jackson <rjackson@linkedin.com>
2017-05-04 22:37:45 -0400

    Use minimatch for matching...

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const symlinkOrCopy = require('symlink-or-copy');
 const Plugin = require('broccoli-plugin');
 const TreeSync = require('tree-sync');
+const minimatch = require("minimatch");
 
 module.exports = class BroccoliConditionalDebug extends Plugin {
   constructor(node, label) {
@@ -44,8 +45,12 @@ function buildDebugOutputPath(label) {
   return debugOutputPath;
 }
 
-function shouldSyncDebugDir(/* label */) {
-  return !!process.env.BROCCOLI_DEBUG;
+function shouldSyncDebugDir(label) {
+  if (!process.env.BROCCOLI_DEBUG) { return false; }
+
+  return minimatch(label, process.env.BROCCOLI_DEBUG);
 }
 
+module.exports._shouldSyncDebugDir = shouldSyncDebugDir;
+module.exports._buildDebugOutputPath = buildDebugOutputPath;
 module.exports._shouldSyncDebugDir = shouldSyncDebugDir;
