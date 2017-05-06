25161659898d9d4a36e97385faa029a329cc7e1a
Robert Jackson <rjackson@linkedin.com>
2017-05-06 11:09:44 -0400

    Make debug tree builder macro function even less work when not debugging.
    When using an instance of `BroccoliConditionalDebug` we incur a few
    I/O operations even when not debugging:
    
    * broccoli creates output path
    * broccoli-conditional-debug deletes the output path
    * broccoli-conditional-debug symlinks the input path to the output path
    
    This change allows the debug tree callback to avoid even that small amount
    of work. When using `BroccoliConditionalDebug.buildDebugCallback` the only
    cost is the overhead is the cost of running minimatch to see if we are
    debugging.

--- tests/index-test.js
+++ tests/index-test.js
@@ -39,6 +39,8 @@ describe('BroccoliConditionalDebug', function(hooks) {
 
   describe('BroccoliConditionalDebug.buildDebugCallback', function() {
     it('returns a callback that builds debug trees with a consistent prefix', function(assert) {
+      process.env.BROCCOLI_DEBUG = '*';
+
       let debugTree = BroccoliConditionalDebug.buildDebugCallback('foo-addon');
 
       let tree1 = debugTree(input.path(), 'addon-tree');
@@ -47,6 +49,39 @@ describe('BroccoliConditionalDebug', function(hooks) {
       let tree2 = debugTree(input.path(), 'vendor-tree');
       assert.equal(tree2.debugLabel, 'foo-addon:vendor-tree');
     });
+
+    it('returns the input tree if debug flag does not match label', co.wrap(function* (assert) {
+      input.write(fixture);
+      let inputPath = input.path();
+
+      let debugTree = BroccoliConditionalDebug.buildDebugCallback('foo-bar');
+      let subject = debugTree(inputPath, 'derp');
+
+      assert.equal(subject, inputPath, 'is equal to the input because the label does not match the BROCCOLI_DEBUG flag');
+
+      let output = yield buildOutput(subject);
+
+      assert.deepEqual(output.read(), fixture, 'final output matches input');
+      assert.deepEqual(debug.read(), { }, 'debug tree output is empty');
+    }));
+
+    it('returns a BroccoliConditionalDebug tree when the BROCCOLI_DEBUG flag matches the label', co.wrap(function* (assert) {
+      input.write(fixture);
+      let inputPath = input.path();
+
+      process.env.BROCCOLI_DEBUG = 'foo-bar:herp';
+
+      let debugTree = BroccoliConditionalDebug.buildDebugCallback('foo-bar');
+      let subject = debugTree(inputPath, 'herp');
+
+      assert.notEqual(subject, inputPath, 'tree2 does not match input because the label matches the BROCCOLI_DEBUG flag');
+      assert.ok(subject instanceof BroccoliConditionalDebug, 'tree2 is a BroccoliConditionalDebug instance');
+
+      let output = yield buildOutput(subject);
+
+      assert.deepEqual(output.read(), fixture, 'final ouptut matches input');
+      assert.deepEqual(debug.read(), { 'foo-bar:herp': fixture }, 'debug tree output matches input');
+    }));
   });
 
   it('should pass through', co.wrap(function* (assert) {
